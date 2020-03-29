054885e69d55ca77abd19460645a69a9bde4df0f
BAL <reancirl@gmail.com>
2020-03-29 21:53:38 +0800

    title update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>DioceseOfIligan | Login</title>
+        <title>Login | Made by : Reancirl Balaba</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="stack/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
         <link href="stack/css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
